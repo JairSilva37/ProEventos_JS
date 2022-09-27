653c443add09cf38e25559c38d704df8b237bea9
jairrss <jairrss@hotmail.com>
2022-09-26 22:35:04 -0300

    Seção 4 Angular Introdução

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEvento.API.Models;
-
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -13,41 +13,23 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        private readonly DataContext _contex;
 
-        private IEnumerable<Evento> _evento = new Evento[]{
-            new Evento()
-            {
-                EventoId = 1,
-                Tema = "Angular 1 suas ações",
-                Local = "Belém",
-                Lote = "1° Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(12).ToString()
-            },            new Evento()
-            {
-                EventoId = 2,
-                Tema = "C# e suas novidades",
-                Local = "São paulo",
-                Lote = "2° Lote",
-                QtdPessoas = 499,
-                DataEvento = DateTime.Now.AddDays(15).ToString()
-            }
-        };
-        public EventoController()
+        public EventoController(DataContext contex)
         {
-
+            _contex= contex;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _contex.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento.Where(x => x.EventoId == id);
+            return _contex.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
 
         [HttpPost]
