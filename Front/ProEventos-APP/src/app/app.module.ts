d90ed7ac7a7affc7a09ea5e198578d049371b269
jairrss <jairrss@hotmail.com>
2022-09-29 11:20:10 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-APP/src/app/app.module.ts
+++ Front/ProEventos-APP/src/app/app.module.ts
@@ -16,13 +16,19 @@ import { AppComponent } from './app.component';
 import{DateTimeFormatPipe} from './helpers/DateTimeFormat.pipe'
 
 import { TituloComponent } from './shared/titulo/titulo.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { EventoService } from './services/evento.service';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
+
 
 
 
@@ -37,7 +43,12 @@ import { NavComponent } from './shared/nav/nav.component';
     EventosComponent,
     PalestrantesComponent,
     NavComponent,
-    DateTimeFormatPipe
+    DateTimeFormatPipe,
+    EventoDetalheComponent,
+    EventoListaComponent,
+    UserComponent,
+    LoginComponent,
+    RegistrationComponent
    ],
   imports: [
     BrowserModule,
